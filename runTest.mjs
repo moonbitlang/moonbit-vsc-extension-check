79a2b90023bd0113a1162b435a46ba156f470a43
文宇祥 <hackwaly@qq.com>
2024-09-29 15:12:33 +0800

    print extension versions

--- runTest.mjs
+++ runTest.mjs
@@ -1,21 +1,47 @@
 import path from "node:path";
 import { runTests, runVSCodeCommand } from "@vscode/test-electron";
-import os from 'node:os';
+import { $ } from "execa";
+import os from "node:os";
 
 const testWorkspace = path.resolve(import.meta.dirname, "./fixtures/hello");
 
 await runVSCodeCommand(["--install-extension", "moonbit.moonbit-lang-nightly"]);
 
+const { stdout } = await runVSCodeCommand([
+  "--list-extensions",
+  "--show-versions",
+]);
+console.log(stdout);
+
 await runTests({
   extensionTestsPath: path.resolve(import.meta.dirname, "./out/prepare.js"),
-  launchArgs: ["--disable-gpu", testWorkspace, '--user-data-dir', `${os.tmpdir()}/vscode-test-profile`],
+  launchArgs: [
+    "--disable-gpu",
+    testWorkspace,
+    "--user-data-dir",
+    `${os.tmpdir()}/vscode-test-profile`,
+  ],
 });
 
 await runTests({
   extensionTestsPath: path.resolve(import.meta.dirname, "./out/index.js"),
   extensionTestsEnv: {
     // This only works for linux
-    PATH: os.platform() === 'win32' ? `${process.env.PATH};${os.homedir()}\\.moon\\bin` : `${process.env.PATH}:${os.homedir()}/.moon/bin`,
+    PATH:
+      os.platform() === "win32"
+        ? `${process.env.PATH};${os.homedir()}\\.moon\\bin`
+        : `${process.env.PATH}:${os.homedir()}/.moon/bin`,
   },
-  launchArgs: ["--disable-gpu", testWorkspace, '--user-data-dir', `${os.tmpdir()}/vscode-test-profile`],
+  launchArgs: [
+    "--disable-gpu",
+    testWorkspace,
+    "--user-data-dir",
+    `${os.tmpdir()}/vscode-test-profile`,
+  ],
 });
+
+const moonPath =
+  os.platform() === "win32"
+    ? `${os.homedir()}\\.moon\\bin\\moon.exe`
+    : `${os.homedir()}/.moon/bin/moon`;
+await $({ stdout: "inherit", verbose: "short" })`${moonPath} version --all`;
